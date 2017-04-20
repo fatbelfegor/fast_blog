0875217ba7ec4efdbbf82e9662dac6a8a65da1e1
Klim Agarkov <jewelgatorstv@gmail.com>
2017-04-20 16:40:44 +0300

    Add rspec capybara tests

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,8 +11,8 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new
-    if @post.save(post_params)
+    @post = Post.new(post_params)
+    if @post.save
       flash[:notice] = "Successfully created post!"
       redirect_to post_path(@post)
     else
@@ -35,7 +35,7 @@ class PostsController < ApplicationController
   end
 
   def show
-    
+    @post = Post.find(params[:id])
   end
 
   def destroy
