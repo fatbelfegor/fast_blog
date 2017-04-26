7afcac3f028f9ab42122d51e81d83e5426444806
Klim Agarkov <jewelgatorstv@gmail.com>
2017-04-26 14:45:19 +0300

    Add some views for index and navbar

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -39,5 +39,4 @@ class AvatarUploader < CarrierWave::Uploader::Base
   def extension_whitelist
     %w(jpg jpeg gif png)
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :admins
   mount Ckeditor::Engine => '/ckeditor'
+
   root to: "posts#index"
   resources :posts
+
+  get 'about' => 'pages#about', as: :about
+  get 'contact' => 'pages#contact', as: :contact
 end
